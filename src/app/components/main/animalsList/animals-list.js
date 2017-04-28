775c9aad7879b57ad880ecfc42138583c17eb52c
fullstack <fullstack2017>
2017-04-28 13:57:24 +0200

    three

--- src/app/components/main/animalsList/animals-list.js
+++ src/app/components/main/animalsList/animals-list.js
@@ -16,7 +16,42 @@
        vm.user = gameFactory.getUser();   
       console.log( vm.user);
       console.log(vm.allAnimals);
+
     };
+
+
+   var name1;
+   var name2;
+     var contador=0;
+
+    vm.goToGame = function(name) {
+      
+      console.log(name);
+        
+      name1=name;
+     
+    
+
+      if(name1===name2 && contador!=0)
+       { vm.allAnimals.splice (1, 2);
+        contador=0;
+  name1=null;
+            name2=null;
+
+       }
+       else{
+        name2=name;
+          contador++;
+
+          if (contador==2)
+          { contador=0;
+            name1=null;
+            name2=null;
+          }
+       }
+
+    };
+
 
     
 
